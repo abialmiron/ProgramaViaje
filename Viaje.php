f744cef9a81f2f7284213b376f3cdfa232ec2b6a
abialmiron <abialmiron@gmail.com>
2023-04-15 12:23:04 -0300

    Modificando el entregable uno, realizando modificaciones en la clase Viaje para luego llevarlas al test

--- Viaje.php
+++ Viaje.php
@@ -52,24 +52,83 @@ class Viaje{
         $this->pasajeros = $pasajeros;
     }
     /*Devuelve el valor actual almacenado en el atributo pasajeros
-    @return array $pasajeros*/
+    * @return array $pasajeros
+    */
     public function getPasajeros(){
         return $this->pasajeros;
     }
 
-    public function __toString(){
-        $mensaje = '';
-        $mensaje .= "Código de viaje: " . $this->getCodViaje() . "\n".  
-        "Destino: " . $this->getDestino() . "\n" .
-        "Cantidad máxima de pasajeros: " . $this->getCantMaximaPasajeros() . "\n";
-        $pasajeros = $this->getPasajeros();
-        for ($i=0;$i < count($pasajeros); $i++){
+    /************ METODOS PROPIOS DE LA CLASE ************/
+
+    /** Este método se encarga de mostrar la colección de pasajeros de una forma mas entendible.
+     *  @return string $mensaje
+     */
+    public function mostrarPasajeros(){
+        $colPasajeros = $this->getPasajeros();
+        for ($i=0;$i < count($colPasajeros); $i++){
             $mensaje .= "Pasajero: " . ($i+1) . "\n";
-            $mensaje .= "DNI: ". $pasajeros[$i]['dni']."\n";
-            $mensaje .= "Nombre: ". $pasajeros[$i]['nombre']."\n";
-            $mensaje .= "Apellido: ". $pasajeros[$i]['apellido']."\n";
+            $mensaje .= "DNI: ". $colPasajeros[$i]['dni']."\n";
+            $mensaje .= "Nombre: ". $colPasajeros[$i]['nombre']."\n";
+            $mensaje .= "Apellido: ". $colPasajeros[$i]['apellido']."\n";
         }
         return $mensaje;
+    }
+    /** Este método se encarga de buscar un pasajero en el arreglo de pasajeros, busca por DNI.
+     * @param int $dniABuscar
+     * @return int $i -> es la posición donde se encuentra ese pasajero con ese DNI. Si no lo encuentra devuelve -1.
+     */
+    public function buscaPasajero($dniABuscar){
+        $colPasajeros = $this->getPasajeros();
+        $i = 0;
+        $encontro = false;
+        $longitud = count($colPasajeros);
+        while($i < $longitud && !$encontro){
+            $encontro = $colPasajeros[$i]['dni'] == $dniABuscar;
+            $i++;
+        }
+        if($encontro){
+            $i--;
+        } else {
+            $i = -1;
+        }
+        return $i;
+    }
+    /** Este método se encarga de modificar el DNI de un pasajero, buscandolo por su DNI.
+     * @param int $dniABuscar
+     * @param int $dniCambio
+     */
+    public function modificarDniPasajero($dniABuscar, $dniCambio){
+        $indice = $this->buscaPasajero($dniABuscar);
+        $colPasajeros = $this->getPasajeros();
+        $colPasajeros[$indice]['dni'] = $dniCambio;
+        $this->setPasajeros($colPasajeros);
+    }
+    /** Este método se encarga de modificar el nombre de un pasajero, buscandolo por su DNI.
+     * @param int $dniDelPasajero
+     * @param string $nuevoNombre
+     */
+    public function modificarNombrePasajero($dniDelPasajero, $nuevoNombre){
+        $indice = $this->buscaPasajero($dniDelPasajero);
+        $colPasajeros = $this->getPasajeros();
+        $colPasajeros[$indice]['nombre'] = $nuevoNombre;
+        $this->setPasajeros($colPasajeros);
+    }
+    /** Este método se encarga de modificar el apellido de un pasajero, buscandolo por su DNI.
+     * @param int $dniDelPasajero
+     * @param string $nuevoApellido
+     */
+    public function modificarApellidoPasajero($dniDelPasajero, $nuevoApellido){
+        $indice = $this->buscaPasajero($dniDelPasajero);
+        $colPasajeros = $this->getPasajeros();
+        $colPasajeros[$indice]['apellido'] = $nuevoApellido;
+        $this->setPasajeros($colPasajeros);
+    }
+    
+    public function __toString(){
+        return "Código de viaje: " . $this->getCodViaje() . "\n".  
+        "Destino: " . $this->getDestino() . "\n" .
+        "Cantidad máxima de pasajeros: " . $this->getCantMaximaPasajeros() . "\n".
+        $this->mostrarPasajeros();
     }   
 }
 ?>
